6e8df30b3bbbb16fc1862e4252d9b806a6dd69e7
naveenrhindi <naveenhindi4@gmail.com>
2025-06-17 16:27:26 +0530

    Initial commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,23 @@
-import './App.css'
+import { ToastContainer } from "react-toastify";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import Login from "./pages/Login";
+import EmailVerify from "./pages/EmailVerify";
+import ResetPassword from "./pages/ResetPassword";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div>
+      <ToastContainer />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/email-verify" element={<EmailVerify />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
+      </Routes>
+    </div>
+  );
+};
 
-export default App
+export default App;
